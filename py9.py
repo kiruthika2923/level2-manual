62f43a85a68f6e5ee45845923d9ef6a1e39ae8a5
kiruthika2923 <kiruthika2912007@gmail.com>
2025-07-31 14:52:13 +0530

    Add files via upload

---
+++ py9.py
@@ -0,0 +1,9 @@
+print("SUM OF N NUMBERS")
+print("----------------")
+n=int(input("enter a n value:"))
+sum=n*(n+1)/2
+print("sum of n values:",sum)
+sums=n*(n+1)*(2*n+1)/6
+print("sum of n square values:",sums)
+sumc=(n*(n+1)/2)**2
+print("sum of n cube values:",sumc)
